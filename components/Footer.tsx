ea70287b8fca83ddca52f63bdd7bb60bfb99b337
12mohamed01234-glitch <12mohamed01234@gmail.com>
2026-02-04 02:17:20 +0200

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="text-center py-4 bg-gray-100 mt-10">
+      © 2026 Zagazig National University
+    </footer>
+  );
+};
+
+export default Footer;
