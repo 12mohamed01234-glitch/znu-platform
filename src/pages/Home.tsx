e29d3b381ef81afeb0a90ebec657910ea32cc663
12mohamed01234-glitch <12mohamed01234@gmail.com>
2026-02-04 02:04:42 +0200

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div
+      style={{
+        padding: "60px",
+        fontSize: "32px",
+        direction: "rtl",
+        fontFamily: "Arial",
+      }}
+    >
+      ✅ ZNU Platform شغّال
+    </div>
+  );
+};
+
+export default Home;
