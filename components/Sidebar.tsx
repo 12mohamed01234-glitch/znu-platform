2d79f5be600bc1c8c4f89e0ed696314b5b9d2557
12mohamed01234-glitch <12mohamed01234@gmail.com>
2026-02-04 14:30:55 +0200

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,17 +1,16 @@
-const Sidebar = () => {
+export default function Sidebar() {
   return (
     <aside className="sidebar">
       <h3>ZNU</h3>
       <ul>
         <li>الرئيسية</li>
-        <li>الكليات والبرامج</li>
-        <li>المكتبة الرقمية</li>
+        <li>الكليات</li>
+        <li>المكتبة</li>
         <li>النتائج</li>
         <li>الشكاوى</li>
       </ul>
-      <button className="login-btn">دخول المنتسبين</button>
+      <button>دخول المنتسبين</button>
     </aside>
   );
-};
+}
 
-export default Sidebar;
