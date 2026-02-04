6070526b81ce37cd6069088e29a8632fa1f4c213
12mohamed01234-glitch <12mohamed01234@gmail.com>
2026-02-04 13:50:08 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Colleges from "./pages/Colleges";
-import Login from "./pages/Login";
+import Home from "./src/pages/Home";
 
 const App = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/colleges" element={<Colleges />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </HashRouter>
   );
