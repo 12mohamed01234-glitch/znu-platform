71f9341e1256bd329ea0d01236f6c8f79bdb7016
12mohamed01234-glitch <12mohamed01234@gmail.com>
2026-02-04 14:13:11 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,18 @@
 import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   return (
     <>
       <Navbar />
 
-      <div className="layout">
-        <Sidebar />
-
-        <div className="hero-wrapper">
-          <div className="hero-card">
-            <h4>مرحباً بكم في</h4>
-            <h1>Zagazig National University</h1>
-            <p>
-              بوابتكم الرقمية المتكاملة للخدمات الأكاديمية والطلابية
-            </p>
-            <button>استكشف الكليات</button>
-          </div>
+      <div className="hero-wrapper">
+        <div className="hero-card">
+          <h4>مرحباً بكم في</h4>
+          <h1>Zagazig National University</h1>
+          <p>
+            بوابتكم الرقمية المتكاملة للخدمات الأكاديمية والطلابية
+          </p>
+          <button>استكشف الكليات</button>
         </div>
       </div>
     </>
