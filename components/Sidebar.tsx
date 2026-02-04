9f45cb8d19b3fecdf38f60a8fc7704d19f48aca6
12mohamed01234-glitch <12mohamed01234@gmail.com>
2026-02-04 13:48:48 +0200

    Create Sidebar.tsx

---
+++ components/Sidebar.tsx
@@ -0,0 +1,17 @@
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <h3>ZNU</h3>
+      <ul>
+        <li>الرئيسية</li>
+        <li>الكليات والبرامج</li>
+        <li>المكتبة الرقمية</li>
+        <li>النتائج</li>
+        <li>الشكاوى</li>
+      </ul>
+      <button className="login-btn">دخول المنتسبين</button>
+    </aside>
+  );
+};
+
+export default Sidebar;
