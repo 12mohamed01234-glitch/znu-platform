fa5bd852da24382f2c0f823cc40b69dbd7e84278
12mohamed01234-glitch <12mohamed01234@gmail.com>
2026-02-04 13:49:13 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,13 @@
 const Navbar = () => {
   return (
     <nav>
-      <div className="logo">ZNU Platform</div>
-
       <div className="links">
         <a href="#/">الرئيسية</a>
-        <a href="#/colleges">الكليات</a>
+        <a href="#/faculties">الكليات</a>
+        <a href="#/library">المكتبة</a>
         <a href="#/login">دخول</a>
       </div>
+      <h2>ZNU Platform</h2>
     </nav>
   );
 };
