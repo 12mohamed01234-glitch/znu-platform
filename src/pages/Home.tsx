59646fc52307ea37ce9a6134ccc9dac589a898ec
12mohamed01234-glitch <12mohamed01234@gmail.com>
2026-02-04 13:58:13 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,27 @@
-<h1 style={{ color: "red" }}>TEST HOME</h1>
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div className="layout">
+        <Sidebar />
+
+        <div className="hero-wrapper">
+          <div className="hero-card">
+            <h4>مرحباً بكم في</h4>
+            <h1>Zagazig National University</h1>
+            <p>
+              بوابتكم الرقمية المتكاملة للخدمات الأكاديمية والطلابية
+            </p>
+            <button>استكشف الكليات</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
